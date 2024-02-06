3954859cafba389113f94f368d67267fdc0dd860
Alejandro Gastelum Flores <alejandro.gastelumf@gmail.com>
2024-02-06 14:39:44 -0800

    fixed next-auth config for old version that I am using

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,63 +1,73 @@
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import express from 'express';
-import http from 'http';
-import typeDefs from './graphql/typeDefs';
-import resolvers from './graphql/resolvers';
-import { getSession } from 'next-auth/react';
-import 'dotenv/config';
+import { ApolloServer } from "apollo-server-express";
+import {
+	ApolloServerPluginDrainHttpServer,
+	ApolloServerPluginLandingPageLocalDefault,
+} from "apollo-server-core";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import express from "express";
+import http from "http";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
+import { getSession } from "next-auth/react";
+import "dotenv/config";
 
 async function main() {
-  console.log(process.env.CLIENT_ORIGIN)
-  // Required logic for integrating with Express
-  const app = express();
-  // Our httpServer handles incoming requests to our Express app.
-  // Below, we tell Apollo Server to "drain" this httpServer,
-  // enabling our servers to shut down gracefully.
-  const httpServer = http.createServer(app);
+	console.log(process.env.CLIENT_ORIGIN);
+	// Required logic for integrating with Express
+	const app = express();
+	// Our httpServer handles incoming requests to our Express app.
+	// Below, we tell Apollo Server to "drain" this httpServer,
+	// enabling our servers to shut down gracefully.
+	const httpServer = http.createServer(app);
 
-  const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers,
-  });
+	const schema = makeExecutableSchema({
+		typeDefs,
+		resolvers,
+	});
 
-  const corsOptions = {
-    origin: process.env.CLIENT_ORIGIN,
-    credentials: true
-  };
+	const corsOptions = {
+		origin: process.env.CLIENT_ORIGIN,
+		credentials: true,
+	};
 
-  // Same ApolloServer initialization as before, plus the drain plugin
-  // for our httpServer.
-  const server = new ApolloServer({
-    schema,
-    csrfPrevention: true,
-    cache: 'bounded',
-    context: async ({ req, res }) => {
-      const session = await getSession({ req });
-      console.log("CONTEXT SESSION", session);
-      
-      return {
-        session
-      };
-    },
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageLocalDefault({ embed: true })],
-  });
+	// Same ApolloServer initialization as before, plus the drain plugin
+	// for our httpServer.
+	const server = new ApolloServer({
+		schema,
+		csrfPrevention: true,
+		cache: "bounded",
+		context: async ({ req, res }) => {
+			const session = await getSession({ req });
+			console.log("CONTEXT SESSION", session);
 
-  // More required logic for integrating with Express
-  await server.start();
-  server.applyMiddleware({
-    app,
-    cors: corsOptions,
-    // By default, apollo-server hosts its GraphQL endpoint at the
-    // server root. However, *other* Apollo Server packages host it at
-    // /graphql. Optionally provide this to match apollo-server.
-    path: '/',
-  });
+			return {
+				session,
+			};
+		},
+		plugins: [
+			ApolloServerPluginDrainHttpServer({ httpServer }),
+			ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+		],
+	});
 
-  // Modified server startup
-  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+	// More required logic for integrating with Express
+	await server.start();
+	server.applyMiddleware({
+		app,
+		cors: corsOptions,
+		// By default, apollo-server hosts its GraphQL endpoint at the
+		// server root. However, *other* Apollo Server packages host it at
+		// /graphql. Optionally provide this to match apollo-server.
+		path: "/",
+	});
+
+	// Modified server startup
+	await new Promise<void>((resolve) =>
+		httpServer.listen({ port: 4000 }, resolve)
+	);
+	console.log(
+		`🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+	);
 }
 
 main().catch((err) => console.log(err));
